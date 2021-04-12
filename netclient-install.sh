34af0b367677e16cb733a7f90062ecb77c3f985b
Alex <31018251+afeiszli@users.noreply.github.com>
2021-04-12 15:42:57 -0400

    Update netclient-install.sh

--- netclient-install.sh
+++ netclient-install.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 set -e
 
-[ -z "$SERVER_URL" ] && echo "Need to set SERVER_URL" && exit 1;
-[ -z "$NET_NAME" ] && echo "Need to set NET_NAME" && exit 1;
 [ -z "$KEY" ] && KEY=nokey;
-
-
 
 wget -O netclient https://github.com/gravitl/netmaker/releases/download/latest/netclient netclient
 chmod +x netclient
