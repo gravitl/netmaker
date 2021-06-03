fe915d931d7fa7cef13cbd3baced0b99866ee696
Alex <31018251+afeiszli@users.noreply.github.com>
2021-06-03 00:40:28 -0400

    Update netclient-install.sh

--- scripts/netclient-install.sh
+++ scripts/netclient-install.sh
@@ -3,7 +3,7 @@ set -e
 
 [ -z "$KEY" ] && KEY=nokey;
 
-wget -O netclient https://github.com/gravitl/netmaker/releases/download/v0.5/netclient
+wget -O netclient https://github.com/gravitl/netmaker/releases/download/v0.5-beta/netclient
 chmod +x netclient
 sudo ./netclient register -t $KEY
 sudo ./netclient join -t $KEY
