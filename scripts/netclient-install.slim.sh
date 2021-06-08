0f03955819dda28d11899ed5f2f12a68e69ae0f2
Alex <31018251+afeiszli@users.noreply.github.com>
2021-06-08 17:06:28 -0400

    Create netclient-install.slim.sh

---
+++ scripts/netclient-install.slim.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+[ -z "$KEY" ] && KEY=nokey;
+
+wget -O netclient https://github.com/gravitl/netmaker/releases/download/v0.5-beta/netclient
+chmod +x netclient
+sudo ./netclient join -t $KEY
+rm -f netclient
