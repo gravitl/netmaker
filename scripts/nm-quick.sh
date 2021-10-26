029a66c8c7f2383055c4dfd37172d84f7ac076b2
afeiszli <alex.feiszli@gmail.com>
2021-10-26 15:16:15 +0000

    removing output from nm-quick wget

--- scripts/nm-quick.sh
+++ scripts/nm-quick.sh
@@ -45,9 +45,6 @@ for dependency in ${dependencies[@]}; do
     fi
 done
 
-
-
-
 set -e
 
 echo "setting public ip values..."
@@ -55,7 +52,7 @@ echo "setting public ip values..."
 NETMAKER_BASE_DOMAIN=nm.$(curl -s ifconfig.me | tr . -).nip.io
 COREDNS_IP=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
 SERVER_PUBLIC_IP=$(curl -s ifconfig.me)
-REPLACE_MASTER_KEY=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 30 ; echo '')
+MASTER_KEY=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 30 ; echo '')
 EMAIL="fake@email.com"
 
 arg1=$( echo $1 | awk -F"domain=" '{print $2}')
@@ -85,24 +82,24 @@ fi
 echo "        domain: $NETMAKER_BASE_DOMAIN"
 echo "    coredns ip: $COREDNS_IP"
 echo "     public ip: $SERVER_PUBLIC_IP"
-echo "    master key: $REPLACE_MASTER_KEY"
+echo "    master key: $MASTER_KEY"
 
 
 echo "setting caddyfile..."
 
 
-wget -O /root/Caddyfile https://raw.githubusercontent.com/gravitl/netmaker/master/docker/Caddyfile
+wget -q -O /root/Caddyfile https://raw.githubusercontent.com/gravitl/netmaker/master/docker/Caddyfile
 sed -i "s/NETMAKER_BASE_DOMAIN/$NETMAKER_BASE_DOMAIN/g" /root/Caddyfile
 sed -i "s/YOUR_EMAIL/$EMAIL/g" /root/Caddyfile
 
 
 echo "setting docker-compose..."
 
-wget -O /root/docker-compose.yml https://raw.githubusercontent.com/gravitl/netmaker/master/compose/docker-compose.caddy.yml
+wget -q -O /root/docker-compose.yml https://raw.githubusercontent.com/gravitl/netmaker/master/compose/docker-compose.caddy.yml
 sed -i "s/NETMAKER_BASE_DOMAIN/$NETMAKER_BASE_DOMAIN/g" /root/docker-compose.yml
 sed -i "s/SERVER_PUBLIC_IP/$SERVER_PUBLIC_IP/g" /root/docker-compose.yml
 sed -i "s/COREDNS_IP/$COREDNS_IP/g" /root/docker-compose.yml
-sed -i "s/REPLACE_MASTER_KEY/$REPLACE_MASTER_KEY/g" /root/docker-compose.yml
+sed -i "s/REPLACE_MASTER_KEY/$MASTER_KEY/g" /root/docker-compose.yml
 
 echo "starting containers..."
 
@@ -126,8 +123,55 @@ cat << "EOF"
 
 EOF
 
-echo "             finished installing"
-echo " "
-echo "             visit dashboard.$NETMAKER_BASE_DOMAIN to log in"
-echo " "
-echo " "
+
+echo "visit dashboard.$NETMAKER_BASE_DOMAIN to log in"
+echo""
+sleep 2
+
+if [ "${NETWORK_SETUP}" == "off" ]; then
+	echo "install complete"
+	exit 0
+fi
+
+echo "creating default network (10.101.0.0/16)"
+
+curl -d '{"addressrange":"10.101.0.0/16","netid":"default"}' -H "Authorization: Bearer $MASTER_KEY" -H 'Content-Type: application/json' localhost:8081/api/networks
+
+sleep 2
+
+echo "creating default key"
+
+curlresponse=$(curl -s -d '{"uses":99999,"name":"defaultkey"}' -H "Authorization: Bearer $MASTER_KEY" -H 'Content-Type: application/json' localhost:8081/api/networks/default/keys)
+ACCESS_TOKEN=$(jq -r '.accessstring' <<< ${curlresponse})
+
+sleep 2
+
+echo "configuring netmaker server as ingress gateway"
+
+curlresponse=$(curl -s -H "Authorization: Bearer $MASTER_KEY" -H 'Content-Type: application/json' localhost:8081/api/nodes/default)
+SERVER_ID=$(jq -r '.[0].macaddress' <<< ${curlresponse})
+
+curl -X POST -H "Authorization: Bearer $MASTER_KEY" -H 'Content-Type: application/json' localhost:8081/api/nodes/default/$SERVER_ID/createingress
+
+echo "finished configuring server and network. You can now add clients."
+echo ""
+echo ""
+echo "For Linux and Mac clients, install with the following command:"
+echo "        curl -sfL https://raw.githubusercontent.com/gravitl/netmaker/develop/scripts/netclient-install.sh | sudo KEY=$ACCESS_TOKEN sh -"
+echo ""
+echo ""
+echo "For Windows clients, perform the following from powershell, as administrator:"
+echo "        1. Make sure WireGuardNT is installed - https://download.wireguard.com/windows-client/wireguard-installer.exe"
+echo "        2. Download netclient.exe - wget https://github.com/gravitl/netmaker/releases/download/latest/netclient.exe"
+echo "        3. Install Netclient - powershell.exe .\\netclient.exe join -t $ACCESS_TOKEN"
+echo "        4. Whitelist C:\ProgramData\Netclient in Windows Defender"
+echo ""
+echo ""
+echo "For Android and iOS clients, perform the following steps:"
+echo "        1. Log into UI at dashboard.$NETMAKER_BASE_DOMAIN"
+echo "        2. Navigate to \"EXTERNAL CLIENTS\" tab"
+echo "        3. Select the gateway and create clients"
+echo "        4. Scan the QR Code from WireGuard app in iOS or Android"
+echo ""
+echo ""
+echo "Netmaker setup is now complete. You are ready to begin using Netmaker."
