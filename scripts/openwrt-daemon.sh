9a3fa2d8f1f624901ca932d0e50a472ca6274922
oycol <oycol527@outlook.com>
2021-11-07 23:54:34 +0800

    openwrt: small fix with pids

--- scripts/openwrt-daemon.sh
+++ scripts/openwrt-daemon.sh
@@ -1,20 +1,23 @@
 #!/bin/bash /etc/rc.common
-# Copyright (C) 2007 OpenWrt.org
+#Created by oycol<oycol527@outlook.com>
+
 EXTRA_COMMANDS="status"
 EXTRA_HELP="        status  	Check service is running"
 START=99
 
-
 LOG_FILE="/tmp/netclient.logs"
 
 start() {
+  if [ ! -f "${LOG_FILE}" ];then
+      touch "${LOG_FILE}"
+  fi
   local PIDS=($(ps -ef|grep "netclient checkin -n all"|grep -v grep|awk '{print $1}'))
-  if [ $PIDS ];then
+  if [ ${PIDS} ];then
     echo "service is running"
     return
   fi
-  bash -c "while [ 1 ]; do /etc/netclient/netclient checkin -n all >> $LOG_FILE 2>&1;sleep 15;\
-           if [ $(ls -l $LOG_FILE|awk '{print $5}') -gt 10240000 ];then tar zcf "${LOG_FILE}.tar" -C / "tmp/netclient.logs"  && > $LOG_FILE;fi;done &"
+  bash -c "while [ 1 ]; do /etc/netclient/netclient checkin -n all >> ${LOG_FILE} 2>&1;sleep 15;\
+           if [ $(ls -l ${LOG_FILE}|awk '{print $5}') -gt 10240000 ];then tar zcf "${LOG_FILE}.tar" -C / "tmp/netclient.logs"  && > ${LOG_FILE};fi;done &"
   echo "start"
 }
 
@@ -28,8 +31,8 @@ stop() {
 
 status() {
   local PIDS=($(ps -ef|grep "netclient checkin -n all"|grep -v grep|awk '{print $1}'))
-  if [ $PIDS ];then
-    echo -e "netclient[$PIDS] is running \n"
+  if [ ${PIDS} ];then
+    echo -e "netclient[${PIDS}] is running \n"
   else
     echo -e "netclient is not running \n"
   fi
