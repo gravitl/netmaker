dc6ee1a18a7fb718cebee232f35911e61a3ae394
afeiszli <alex.feiszli@gmail.com>
2021-11-29 05:28:08 -0500

    adding templates

--- scripts/netclient.sh
+++ scripts/netclient.sh
@@ -16,9 +16,12 @@ if [ $? -ne 0 ]; then { echo "Failed to join, quitting." ; exit 1; } fi
 
 echo "[netclient] Starting netclient checkin"
 # loop and call checkin -n all
+FAILCOUNT=0
 while [ 1 ]; do
     # add logs to netclient.logs
     /root/netclient checkin -n all
+    if [ $? -ne 0 ]; then FAILCOUNT=$((FAILCOUNT+1)) ; else FAILCOUNT=0; fi
+    if [ $FAILCOUNT -gt 2 ]; then { echo "Failing checkins frequently, restarting." ; exit 1; } fi
     sleep $SLEEP
 done
 echo "[netclient] exiting"
