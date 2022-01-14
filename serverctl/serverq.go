f9b3d46c20729b5c94d75534576ad6bf2f9b0754
0xdcarns <dillon.carns@gmail.com>
2022-01-14 10:33:14 -0500

    added server action queue

---
+++ serverctl/serverq.go
@@ -0,0 +1,36 @@
+package serverctl
+
+import (
+	"fmt"
+
+	"github.com/gravitl/netmaker/models"
+)
+
+// ServerQueue - holds data to be updated across the server
+var ServerQueue chan ServerUpdateData
+
+func init() {
+	ServerQueue = make(chan ServerUpdateData, 100)
+}
+
+// ServerUpdateData - contains data to configure server
+// and if it should set peers
+type ServerUpdateData struct {
+	RemakePeers bool        `json:"remakepeers" bson:"remakepeers"`
+	ServerNode  models.Node `json:"servernode" bson:"servernode"`
+}
+
+// Push - Pushes ServerUpdateData to be used later
+func Push(serverData ServerUpdateData) {
+	ServerQueue <- serverData
+}
+
+// Pop - fetches first available data from queue
+func Pop() (ServerUpdateData, error) {
+	select {
+	case serverData := <-ServerQueue:
+		return serverData, nil
+	default:
+		return ServerUpdateData{}, fmt.Errorf("empty server queue")
+	}
+}
