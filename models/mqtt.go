05dd87887eb06bfe1e6398b2af769359243f606c
Matthew R Kasun <mkasun@nusak.ca>
2022-01-15 18:16:44 -0500

    moved peer determination to server

--- models/mqtt.go
+++ models/mqtt.go
@@ -1,9 +1,10 @@
 package models
 
+import "golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+
 type PeerUpdate struct {
-	Network  string
-	Nodes    []Node
-	ExtPeers []ExtPeersResponse
+	Network string
+	Peers   []wgtypes.PeerConfig
 }
 
 type KeyUpdate struct {
