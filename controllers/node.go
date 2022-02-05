c22f35f57ff5cd8d973786a99e30b0b6b31fa58c
0xdcarns <dillon.carns@gmail.com>
2022-02-05 13:26:12 -0500

    added better check

--- controllers/node.go
+++ controllers/node.go
@@ -610,7 +610,7 @@ func deleteNode(w http.ResponseWriter, r *http.Request) {
 func runUpdates(node *models.Node, nodeUpdate bool) error {
 	//don't publish to server node
 
-	if nodeUpdate && !isServer(node) && servercfg.IsMessageQueueBackend() {
+	if nodeUpdate && !isServer(node) {
 		if err := mq.NodeUpdate(node); err != nil {
 			logger.Log(1, "error publishing node update", err.Error())
 			return err
