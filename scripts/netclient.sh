0d663abf2843d6252bb33f5172a9742edce9558c
afeiszli <alex.feiszli@gmail.com>
2022-02-08 23:55:45 -0500

    netclient container startup script mod

--- scripts/netclient.sh
+++ scripts/netclient.sh
@@ -14,14 +14,8 @@ fi
 /root/netclient join $TOKEN_CMD -daemon off -dnson no -udpholepunch no
 if [ $? -ne 0 ]; then { echo "Failed to join, quitting." ; exit 1; } fi
 
-echo "[netclient] Starting netclient checkin"
-# loop and call checkin -n all
-FAILCOUNT=0
-while [ 1 ]; do
-    # add logs to netclient.logs
-    /root/netclient checkin -n all
-    if [ $? -ne 0 ]; then FAILCOUNT=$((FAILCOUNT+1)) ; else FAILCOUNT=0; fi
-    if [ $FAILCOUNT -gt 2 ]; then { echo "Failing checkins frequently, restarting." ; exit 1; } fi
-    sleep $SLEEP
-done
+echo "[netclient] Starting netclient daemon"
+
+/root/netclient daemon
+
 echo "[netclient] exiting"
