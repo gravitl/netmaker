c6e00d93bdd955b131fc0e3b26325e7ba9190af6
0xdcarns <dillon.carns@gmail.com>
2022-02-10 18:57:00 -0500

    first draft of chunks

--- netclient/ncutils/util.go
+++ netclient/ncutils/util.go
@@ -2,6 +2,7 @@ package ncutils
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -21,4 +22,52 @@ func BackOff(isExponential bool, maxTime int, f interface{}) (interface{}, error
 		PrintLog("retrying...", 1)
 	}
 	return nil, fmt.Errorf("could not find result")
+}
+
+// DestructMessage - reconstruct original message through chunks
+func DestructMessage(builtMsg string, senderPublicKey *[32]byte, recipientPrivateKey *[32]byte) ([]byte, error) {
+	var chunks = strings.Split(builtMsg, splitKey)
+	var totalMessage = make([]byte, len(builtMsg))
+	for _, chunk := range chunks {
+		var bytes, decErr = BoxDecrypt([]byte(chunk), senderPublicKey, recipientPrivateKey)
+		if decErr != nil || bytes == nil {
+			return nil, decErr
+		}
+		totalMessage = append(totalMessage, bytes...)
+	}
+	return totalMessage, nil
+}
+
+// BuildMessage Build a message for publishing
+func BuildMessage(originalMessage []byte, recipientPubKey *[32]byte, senderPrivateKey *[32]byte) (string, error) {
+	chunks := getSliceChunks(originalMessage, 16128)
+	var sb strings.Builder
+	for i := 0; i < len(chunks); i++ {
+		var encryptedText, encryptErr = BoxEncrypt(chunks[i], recipientPubKey, senderPrivateKey)
+		if encryptErr != nil {
+			return "", encryptErr
+		}
+		sb.Write(encryptedText)
+		if i < len(chunks)-1 {
+			sb.WriteString(splitKey)
+		}
+	}
+	return sb.String(), nil
+}
+
+var splitKey = "<|#|>"
+
+func getSliceChunks(slice []byte, chunkSize int) [][]byte {
+	var chunks [][]byte
+	for i := 0; i < len(slice); i += chunkSize {
+		lastByte := i + chunkSize
+
+		if lastByte > len(slice) {
+			lastByte = len(slice)
+		}
+
+		chunks = append(chunks, slice[i:lastByte])
+	}
+
+	return chunks
 }
