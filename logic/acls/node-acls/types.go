d404d1e84db2f51727017bd0cb913cfb7fe8eab0
0xdcarns <dillon.carns@gmail.com>
2022-02-25 11:06:03 -0500

    began generalizing

--- logic/acls/node-acls/types.go
+++ logic/acls/node-acls/types.go
@@ -1,27 +1,12 @@
 package nodeacls
 
-var (
-	// NotPresent - 0 - not present (default)
-	NotPresent = byte(0)
-	// NotAllowed - 1 - not allowed access
-	NotAllowed = byte(1) // 1 - not allowed
-	// Allowed - 2 - allowed access
-	Allowed = byte(2)
+import (
+	"github.com/gravitl/netmaker/logic/acls"
 )
 
 type (
-	// NodeID - the node id of a given node
-	NodeID string
-
-	// NetworkID - the networkID of a given network
-	NetworkID string
-
-	// NodeACL - the ACL of other nodes in a NetworkACL for a single unique node
-	NodeACL map[NodeID]byte
-
-	// NetworkACL - the total list of all node's ACL in a given network
-	NetworkACL map[NodeID]NodeACL
-
-	// ACLJson - the string representation in JSON of an ACL Node or Network
-	ACLJson string
+	// NodeID - node ID for ACLs
+	NodeID acls.AclID
+	// NetworkID - ACL container based on network ID for nodes
+	NetworkID acls.ContainerID
 )
