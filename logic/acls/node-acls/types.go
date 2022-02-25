68c70148021ca544c178602a4b524c754df574fe
0xdcarns <dillon.carns@gmail.com>
2022-02-25 14:08:44 -0500

    refactored node functions further

--- logic/acls/node-acls/types.go
+++ logic/acls/node-acls/types.go
@@ -5,6 +5,8 @@ import (
 )
 
 type (
+	// NodeACL - interface for NodeACLs
+	NodeACL acls.ACL
 	// NodeID - node ID for ACLs
 	NodeID acls.AclID
 	// NetworkID - ACL container based on network ID for nodes
