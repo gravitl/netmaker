bdd8de56fbe598e5fb0e8bc1151d3b321bd131a8
Matthew R. Kasun <mkasun@nusak.ca>
2022-03-04 13:36:48 -0500

    enable CGO for netmaker

--- scripts/build-binaries.sh
+++ scripts/build-binaries.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #server build
-env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-X 'main.version=$VERSION'" -o netclient/build/netmaker main.go
+env CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -ldflags="-X 'main.version=$VERSION'" -o netclient/build/netmaker main.go
 
 cd netclient
 
