d82f9c1c24270225fcd7df030ed75883e2fc3c34
John Sahhar <john@gravitl.com>
2022-03-08 16:25:22 -0600

    config/config_test: add test
    * Adds a test for parsing the dev.yaml config included in the repository
    * Changes readConfig() to return an error in addition to the envrionment
      config
    
    Signed-off-by: John Sahhar <john@gravitl.com>

--- config/config_test.go
+++ config/config_test.go
@@ -1,8 +1,36 @@
+//Environment file for getting variables
+//Currently the only thing it does is set the master password
+//Should probably have it take over functions from OS such as port and mongodb connection details
+//Reads from the config/environments/dev.yaml file by default
 package config
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestReadConfig(t *testing.T) {
-	config := readConfig()
-	t.Log(config)
+func Test_readConfig(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    *EnvironmentConfig
+		wantErr bool
+	}{
+		{
+			"ensure development config parses",
+			&EnvironmentConfig{},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := readConfig()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("readConfig() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("readConfig() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
