eb974dbd63dabc634ef8c8c240fa0b74ed499f70
john s <john@gravitl.com>
2022-03-18 14:06:48 -0500

    config/config_test: add test (#882)
    * Adds a test for parsing the dev.yaml config included in the repository
    * Changes readConfig() to return an error in addition to the envrionment
      config
    
    Signed-off-by: John Sahhar <john@gravitl.com>

--- config/config.go
+++ config/config.go
@@ -26,6 +26,7 @@ func getEnv() string {
 
 // Config : application config stored as global variable
 var Config *EnvironmentConfig
+var SetupErr error
 
 // EnvironmentConfig - environment conf struct
 type EnvironmentConfig struct {
@@ -90,25 +91,26 @@ type SQLConfig struct {
 }
 
 // reading in the env file
-func readConfig() *EnvironmentConfig {
-	file := fmt.Sprintf("config/environments/%s.yaml", getEnv())
+func readConfig() (*EnvironmentConfig, error) {
+	file := fmt.Sprintf("environments/%s.yaml", getEnv())
 	f, err := os.Open(file)
 	var cfg EnvironmentConfig
 	if err != nil {
-		return &cfg
+		return &cfg, err
 	}
 	defer f.Close()
 
 	decoder := yaml.NewDecoder(f)
-	err = decoder.Decode(&cfg)
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(2)
+	if decoder.Decode(&cfg) != nil {
+		return &cfg, err
 	}
-	return &cfg
+	return &cfg, err
 
 }
 
 func init() {
-	Config = readConfig()
+	if Config, SetupErr = readConfig(); SetupErr != nil {
+		log.Fatal(SetupErr)
+		os.Exit(2)
+	}
 }
