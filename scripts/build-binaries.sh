e1ace8850858fa65a783c13ec278d5e04b177f16
john s <john@gravitl.com>
2022-03-22 14:14:57 -0600

    scripts/build-binaries.sh: support for all OS/ARCH (#948)
    * scripts/build-binaries.sh: support for all OS/ARCH
    
    Expands our build scripts to support the following OS/ARCHS:
    
    netclient-linux-amd64
    netclient-linux-arm5
    netclient-linux-arm6
    netclient-linux-arm7
    netclient-linux-arm64
    netclient-linux-mips
    netclient-linux-mips64
    netclient-linux-mips64le
    netclient-linux-mipsle
    netclient-linux-ppc64
    netclient-linux-ppc64le
    netclient-linux-riscv64
    netclient-linux-s390x
    netclient-linux-386
    netclient-freebsd-amd64
    netclient-freebsd-arm5
    netclient-freebsd-arm6
    netclient-freebsd-arm7
    netclient-freebsd-arm64
    netclient-freebsd-386
    netclient-darwin-arm64
    netclient-darwin-amd64
    netclient-windows-amd64
    netclient-windows-arm5
    netclient-windows-arm6
    netclient-windows-arm7
    netclient-windows-arm64
    netclient-windows-386
    
    * bin-maker.sh: update default VERSION
    
    Signed-off-by: John Sahhar <john@gravitl.com>

--- scripts/build-binaries.sh
+++ scripts/build-binaries.sh
@@ -4,18 +4,5 @@
 env CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -ldflags="-X 'main.version=$VERSION'" -o netclient/build/netmaker main.go
 
 cd netclient
+./bin-maker.sh
 
-#client build
-env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-X 'main.version=$VERSION'" -o build/netclient main.go
-env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=5 go build -ldflags="-X 'main.version=$VERSION'" -o build/netclient-arm5 main.go
-env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build -ldflags="-X 'main.version=$VERSION'" -o build/netclient-arm6 main.go
-env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags="-X 'main.version=$VERSION'" -o build/netclient-arm7 main.go
-env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags="-X 'main.version=$VERSION'" -o build/netclient-arm64 main.go
-env CGO_ENABLED=0 GOOS=linux GOARCH=mipsle go build -ldflags "-s -w -X 'main.version=$VERSION'" -o build/netclient-mipsle main.go && upx build/netclient-mipsle
-env CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build  -ldflags="-X 'main.version=$VERSION'" -o build/netclient-freebsd main.go
-env CGO_ENABLED=0 GOOS=freebsd GOARCH=arm GOARM=5 go build -ldflags="-X 'main.version=$VERSION'" -o build/netclient-freebsd-arm5 main.go
-env CGO_ENABLED=0 GOOS=freebsd GOARCH=arm GOARM=6 go build -ldflags="-X 'main.version=$VERSION'" -o build/netclient-freebsd-arm6 main.go
-env CGO_ENABLED=0 GOOS=freebsd GOARCH=arm GOARM=7 go build -ldflags="-X 'main.version=$VERSION'" -o build/netclient-freebsd-arm7 main.go
-env CGO_ENABLED=0 GOOS=freebsd GOARCH=arm64 go build -ldflags="-X 'main.version=$VERSION'" -o build/netclient-freebsd-arm64 main.go
-env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-X 'main.version=$VERSION'" -o build/netclient-darwin main.go
-env CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags="-X 'main.version=${VERSION}'" -o build/netclient-darwin-arm64 main.go
