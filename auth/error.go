1e77a9eca463dd702ceef0697ad5378c113e40a4
9to1url <9to1url@gmail.com>
2022-04-09 16:00:49 -0400

    remove new line to fix warning:  fmt.Fprintln arg list ends with redundant newline

--- auth/error.go
+++ auth/error.go
@@ -6,5 +6,4 @@ const oauthNotConfigured = `<!DOCTYPE html><html>
 <h3>Your Netmaker server does not have OAuth configured.</h3>
 <p>Please visit the docs <a href="https://docs.netmaker.org/oauth.html" target="_blank" rel="noopener">here</a> to learn how to.</p>
 </body>
-</html>
-`
+</html>`
