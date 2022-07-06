c99e6bf75afae93f9669ec6664036ab282d40afe
afeiszli <alex.feiszli@gmail.com>
2022-07-06 13:26:11 -0400

    changes for macos installer

--- netclient/ncutils/netclientutils_darwin.go
+++ netclient/ncutils/netclientutils_darwin.go
@@ -1,19 +1,27 @@
 package ncutils
 
 import (
-	"github.com/gravitl/netmaker/logger"
 	"os/exec"
 	"strings"
+
+	"github.com/gravitl/netmaker/logger"
 )
+
+const WHITESPACE_PLACEHOLDER = "+-+-+-+"
 
 // RunCmd - runs a local command
 func RunCmd(command string, printerr bool) (string, error) {
+
 	args := strings.Fields(command)
+	// return whitespace after split
+	for i, arg := range args {
+		args[i] = strings.Replace(arg, WHITESPACE_PLACEHOLDER, " ", -1)
+	}
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Wait()
 	out, err := cmd.CombinedOutput()
 	if err != nil && printerr {
-		logger.Log(0, "error running command:", command)
+		logger.Log(0, "error running command:", strings.Join(args, " "))
 		logger.Log(0, strings.TrimSuffix(string(out), "\n"))
 	}
 	return string(out), err
