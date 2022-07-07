00f33af10425e1661a291517a4a79dd481380119
0xdcarns <dillon.carns@gmail.com>
2022-07-07 13:36:14 -0400

    changed requiredversion to requiredversions

--- netclient/functions/upgrades/v0-14-5.go
+++ netclient/functions/upgrades/v0-14-5.go
@@ -3,9 +3,9 @@ package upgrades
 import "github.com/gravitl/netmaker/netclient/config"
 
 var upgrade0145 = UpgradeInfo{
-	RequiredVersion: "0.14.4",
-	NewVersion:      "0.14.5",
-	OP:              update0145,
+	RequiredVersions: []string{"0.14.1", "0.14.2", "0.14.3", "0.14.4"},
+	NewVersion:       "0.14.5",
+	OP:               update0145,
 }
 
 func update0145(cfg *config.ClientConfig) {
