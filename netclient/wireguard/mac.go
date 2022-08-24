72060bc587c6310c43e0dbfba4d9d88fb86f3ac2
afeiszli <alex.feiszli@gmail.com>
2022-08-24 16:01:25 -0400

    reverting mac paths

--- netclient/wireguard/mac.go
+++ netclient/wireguard/mac.go
@@ -209,8 +209,8 @@ func addRoute(addr string, iface string) error {
 // setConfig - sets configuration of the wireguard interface from the config file
 func setConfig(realIface string, confPath string) error {
 	confString := getConfig(confPath)
-	pathFormatted := strings.Replace(confPath, " ", "\\ ", -1)
-	err := os.WriteFile(pathFormatted+".tmp", []byte(confString), 0600)
+	// pathFormatted := strings.Replace(confPath, " ", "\\ ", -1)
+	err := os.WriteFile(confPath+".tmp", []byte(confString), 0600)
 	if err != nil {
 		return err
 	}
@@ -221,9 +221,9 @@ func setConfig(realIface string, confPath string) error {
 
 // getConfig - gets config from config file and strips out incompatible fields
 func getConfig(path string) string {
-	pathFormatted := strings.Replace(path, " ", "\\ ", -1)
+	//pathFormatted := strings.Replace(path, " ", "\\ ", -1)
 	var confCmd = "grep -v -e Address -e MTU -e PostUp -e PostDown "
-	confRaw, _ := ncutils.RunCmd(confCmd+pathFormatted, false)
+	confRaw, _ := ncutils.RunCmd(confCmd+path, false)
 	return confRaw
 }
 
