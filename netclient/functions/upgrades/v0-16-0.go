be2fa030c308dc4db5c29d116dd8bbb39af7c637
0xdcarns <dillon.carns@gmail.com>
2022-09-19 09:46:09 -0400

    added 0.16.0 upgrade logic

---
+++ netclient/functions/upgrades/v0-16-0.go
@@ -0,0 +1,21 @@
+package upgrades
+
+import (
+	"github.com/gravitl/netmaker/netclient/config"
+)
+
+var upgrade0160 = UpgradeInfo{
+	RequiredVersions: []string{
+		"v0.15.1",
+		"v0.15.2",
+	},
+	NewVersion: "v0.16.0",
+	OP:         update0160,
+}
+
+func update0160(cfg *config.ClientConfig) {
+	// set connect default if not present 15.X -> 16.0
+	if cfg.Node.Connected == "" {
+		cfg.Node.SetDefaultConnected()
+	}
+}
