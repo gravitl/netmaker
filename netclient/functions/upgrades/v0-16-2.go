3bb19ea1a3e03b93acb3de4657c606ddf516b069
Matthew R. Kasun <mkasun@nusak.ca>
2022-10-18 10:42:02 -0400

    fix 0.16.2 update function

--- netclient/functions/upgrades/v0-16-2.go
+++ netclient/functions/upgrades/v0-16-2.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gravitl/netmaker/netclient/config"
 )
 
-var upgrade0161 = UpgradeInfo{
+var upgrade0162 = UpgradeInfo{
 	RequiredVersions: []string{
 		"v0.14.6",
 		"v0.15.0",
@@ -12,13 +12,11 @@ var upgrade0161 = UpgradeInfo{
 		"v0.15.2",
 		"v0.16.1",
 	},
-	NewVersion: "v0.16.1",
-	OP:         update0161,
+	NewVersion: "v0.16.2",
+	OP:         update0162,
 }
 
-func update0161(cfg *config.ClientConfig) {
+func update0162(cfg *config.ClientConfig) {
 	// set connect default if not present 15.X -> 16.0
-	if cfg.Node.Connected == "" {
-		cfg.Node.SetDefaultConnected()
-	}
+	update0161(cfg)
 }
