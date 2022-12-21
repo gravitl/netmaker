b07504c28643703a8dc88f33a2d6a97b6722e1cc
Matthew R. Kasun <mkasun@nusak.ca>
2022-12-21 13:01:32 -0500

    fix logic

--- logic/nodes.go
+++ logic/nodes.go
@@ -230,9 +230,9 @@ func CreateNode(node *models.Node) error {
 				return err
 			}
 			node.Address.Mask = net.CIDRMask(cidr.Mask.Size())
-		} else if !IsIPUnique(node.Network, node.Address.String(), database.NODES_TABLE_NAME, false) {
-			return fmt.Errorf("invalid address: ipv4 " + node.Address.String() + " is not unique")
 		}
+	} else if !IsIPUnique(node.Network, node.Address.String(), database.NODES_TABLE_NAME, false) {
+		return fmt.Errorf("invalid address: ipv4 " + node.Address.String() + " is not unique")
 	}
 
 	if node.Address6.IP == nil {
@@ -245,9 +245,9 @@ func CreateNode(node *models.Node) error {
 				return err
 			}
 			node.Address6.Mask = net.CIDRMask(cidr.Mask.Size())
-		} else if !IsIPUnique(node.Network, node.Address6.String(), database.NODES_TABLE_NAME, true) {
-			return fmt.Errorf("invalid address: ipv6 " + node.Address6.String() + " is not unique")
 		}
+	} else if !IsIPUnique(node.Network, node.Address6.String(), database.NODES_TABLE_NAME, true) {
+		return fmt.Errorf("invalid address: ipv6 " + node.Address6.String() + " is not unique")
 	}
 
 	node.ID = uuid.New()
