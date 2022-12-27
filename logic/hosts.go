d5446fb30237e84dfb5759c19f14c5ae0d45b37d
Abhishek Kondur <abhi281342@gmail.com>
2022-12-27 19:47:30 +0530

    fix node removal from host

--- logic/hosts.go
+++ logic/hosts.go
@@ -235,15 +235,17 @@ func DissasociateNodeFromHost(n *models.Node, h *models.Host) error {
 			break
 		}
 	}
-
-	if index == -1 {
-		return fmt.Errorf("node %s, not found in host, %s", n.ID.String(), h.ID.String())
+	if index < 0 {
+		if len(h.Nodes) == 0 {
+			return fmt.Errorf("node %s, not found in host, %s", n.ID.String(), h.ID.String())
+		}
+	} else {
+		h.Nodes = RemoveStringSlice(h.Nodes, index)
 	}
-
 	if err := deleteNodeByID(n); err != nil {
 		return err
 	}
-	h.Nodes = RemoveStringSlice(h.Nodes, index)
+
 	return UpsertHost(h)
 }
 
