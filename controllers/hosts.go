d31f9e8eb56d3dba37082946ab43157f839b7512
Abhishek Kondur <abhi281342@gmail.com>
2023-01-17 13:18:51 +0530

    publish host update from server to client

--- controllers/hosts.go
+++ controllers/hosts.go
@@ -108,7 +108,11 @@ func updateHost(w http.ResponseWriter, r *http.Request) {
 			logger.Log(0, r.Header.Get("user"), "failed to update host networks roles in DynSec:", err.Error())
 		}
 	}
-	// TODO: publish host update through MQ
+	// publish host update through MQ
+	mq.HostUpdate(&models.HostUpdate{
+		Action: models.UpdateHost,
+		Host:   *newHost,
+	})
 	go func() {
 		if err := mq.PublishPeerUpdate(); err != nil {
 			logger.Log(0, "fail to publish peer update: ", err.Error())
