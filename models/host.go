e77bd39f44c5e0d6bf9c0fbd155af1a22038f3e7
Matthew R Kasun <mkasun@nusak.ca>
2023-01-20 06:04:50 -0500

    remove localaddress from host

--- models/host.go
+++ models/host.go
@@ -24,7 +24,6 @@ type Host struct {
 	Interface        string           `json:"interface" yaml:"interface"`
 	Debug            bool             `json:"debug" yaml:"debug"`
 	ListenPort       int              `json:"listenport" yaml:"listenport"`
-	LocalAddress     net.IPNet        `json:"localaddress" yaml:"localaddress"`
 	PublicListenPort int              `json:"public_listen_port" yaml:"public_listen_port"`
 	LocalListenPort  int              `json:"locallistenport" yaml:"locallistenport"`
 	ProxyListenPort  int              `json:"proxy_listen_port" yaml:"proxy_listen_port"`
