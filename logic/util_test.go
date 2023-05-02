6d6ba3aa7d24b5fc277109b258431d97f8ddb157
Matthew R Kasun <mkasun@nusak.ca>
2023-05-02 13:28:00 -0400

    random string
    update algorithm
    consolidate MakeRandomString, RandomString & GenerateCrytoString into
    one function

--- logic/util_test.go
+++ logic/util_test.go
@@ -1,4 +1,4 @@
-package ncutils
+package logic
 
 import (
 	"strings"
@@ -7,10 +7,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMakeRandomString(t *testing.T) {
+func TestRandomString(t *testing.T) {
 	for testCase := 0; testCase < 100; testCase++ {
 		for size := 2; size < 2058; size++ {
-			if length := len(MakeRandomString(size)); length != size {
+			if length := len(RandomString(size)); length != size {
 				t.Fatalf("expected random string of size %d, got %d instead", size, length)
 			}
 		}
@@ -18,9 +18,9 @@ func TestMakeRandomString(t *testing.T) {
 }
 
 func TestMakeRandomStringValid(t *testing.T) {
-	lengthStr := MakeRandomString(10)
+	lengthStr := RandomString(10)
 	assert.Equal(t, len(lengthStr), 10)
-	validMqID := MakeRandomString(23)
+	validMqID := RandomString(23)
 	assert.False(t, strings.Contains(validMqID, "#"))
 	assert.False(t, strings.Contains(validMqID, "!"))
 	assert.False(t, strings.Contains(validMqID, "\""))
