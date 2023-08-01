0c70c4daba7d85e5e11eabee0b4b3a542eabfff9
Matthew R Kasun <mkasun@nusak.ca>
2023-08-01 10:50:43 -0400

    enforce unique names for ext client names (#2476)
    * enforce unique names for ext client names
    
    * only check for unique id on creation
    
    * check for unique id if changed

--- logic/clients.go
+++ logic/clients.go
@@ -1,6 +1,7 @@
 package logic
 
 import (
+	"errors"
 	"sort"
 
 	"github.com/gravitl/netmaker/models"
@@ -69,4 +70,18 @@ func SortExtClient(unsortedExtClient []models.ExtClient) {
 	sort.Slice(unsortedExtClient, func(i, j int) bool {
 		return unsortedExtClient[i].ClientID < unsortedExtClient[j].ClientID
 	})
+}
+
+// GetExtClientByName - gets an ext client by name
+func GetExtClientByName(ID string) (models.ExtClient, error) {
+	clients, err := GetAllExtClients()
+	if err != nil {
+		return models.ExtClient{}, err
+	}
+	for i := range clients {
+		if clients[i].ClientID == ID {
+			return clients[i], nil
+		}
+	}
+	return models.ExtClient{}, errors.New("client not found")
 }
