76738702cd4b4f26127d7d0734710834ebbc78eb
gabrielseibel1 <gabrielseibel1@gmail.com>
2023-08-02 08:48:37 -0300

    Rename var

--- controllers/limits.go
+++ controllers/limits.go
@@ -16,27 +16,27 @@ const (
 	clients_l  = 3
 )
 
-func checkFreeTierLimits(limit_choice int, next http.Handler) http.HandlerFunc {
+func checkFreeTierLimits(limitChoice int, next http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var errorResponse = models.ErrorResponse{
 			Code: http.StatusForbidden, Message: "free tier limits exceeded on networks",
 		}
 
 		if logic.Free_Tier { // check that free tier limits not exceeded
-			if limit_choice == networks_l {
+			if limitChoice == networks_l {
 				currentNetworks, err := logic.GetNetworks()
 				if (err != nil && !database.IsEmptyRecord(err)) || len(currentNetworks) >= logic.Networks_Limit {
 					logic.ReturnErrorResponse(w, r, errorResponse)
 					return
 				}
-			} else if limit_choice == users_l {
+			} else if limitChoice == users_l {
 				users, err := logic.GetUsers()
 				if (err != nil && !database.IsEmptyRecord(err)) || len(users) >= logic.Users_Limit {
 					errorResponse.Message = "free tier limits exceeded on users"
 					logic.ReturnErrorResponse(w, r, errorResponse)
 					return
 				}
-			} else if limit_choice == clients_l {
+			} else if limitChoice == clients_l {
 				clients, err := logic.GetAllExtClients()
 				if (err != nil && !database.IsEmptyRecord(err)) || len(clients) >= logic.Clients_Limit {
 					errorResponse.Message = "free tier limits exceeded on external clients"
