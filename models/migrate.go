7da2e9b9bfb56028456c3a9d1001d7df1d40221d
Matthew R Kasun <mkasun@nusak.ca>
2023-08-04 14:11:46 -0400

    restruct migration

--- models/migrate.go
+++ models/migrate.go
@@ -2,6 +2,6 @@ package models
 
 // MigrationData struct needed to create new v0.18.0 node from v.0.17.X node
 type MigrationData struct {
-	NewHost     Host
+	HostName    string
 	LegacyNodes []LegacyNode
 }
