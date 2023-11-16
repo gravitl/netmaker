1bf8253207b414e275ec3095babdedbf9020c868
Abhishek Kondur <abhi281342@gmail.com>
2023-11-16 10:14:15 +0400

    failOver with peer pub key

--- pro/controllers/failover.go
+++ pro/controllers/failover.go
@@ -8,14 +8,14 @@ import (
 	controller "github.com/gravitl/netmaker/controllers"
 	"github.com/gravitl/netmaker/logger"
 	"github.com/gravitl/netmaker/logic"
+	"github.com/gravitl/netmaker/models"
 	"github.com/gravitl/netmaker/mq"
 	proLogic "github.com/gravitl/netmaker/pro/logic"
 	"golang.org/x/exp/slog"
 )
 
 type FailOverMeReq struct {
-	MyNodeID   string `json:"my_node_id"`
-	PeerNodeID string `json:"peer_node_id"`
+	PeerPubKey string `json:"peer_pub_key"`
 }
 
 // RelayHandlers - handle Pro Relays
@@ -36,11 +36,11 @@ func FailOverHandler(r *mux.Router) {
 //				200: nodeResponse
 func failOverME(w http.ResponseWriter, r *http.Request) {
 	var params = mux.Vars(r)
-	nodeid := params["nodeid"]
+	hostid := params["hostid"]
 	// confirm host exists
-	node, err := logic.GetNodeByID(nodeid)
+	host, err := logic.GetHost(hostid)
 	if err != nil {
-		logger.Log(0, r.Header.Get("user"), "failed to get node:", err.Error())
+		logger.Log(0, r.Header.Get("user"), "failed to get host:", err.Error())
 		logic.ReturnErrorResponse(w, r, logic.FormatError(err, "badrequest"))
 		return
 	}
@@ -51,45 +51,61 @@ func failOverME(w http.ResponseWriter, r *http.Request) {
 	var failOverReq FailOverMeReq
 	err = json.NewDecoder(r.Body).Decode(&failOverReq)
 	if err != nil {
-		slog.Error("error decoding request body: ", "error", err.Error())
+		logger.Log(0, r.Header.Get("user"), "error decoding request body: ", err.Error())
 		logic.ReturnErrorResponse(w, r, logic.FormatError(err, "badrequest"))
 		return
 	}
+	var sendPeerUpdate bool
 	allNodes, err := logic.GetAllNodes()
 	if err != nil {
 		slog.Error("failed to get all nodes", "error", err)
 	}
-	peerNode, err := logic.GetNodeByID(failOverReq.PeerNodeID)
+	peerHost, err := logic.GetHostByPubKey(failOverReq.PeerPubKey)
 	if err != nil {
-		slog.Error("failed to get node", "error", err.Error())
-		logic.ReturnErrorResponse(w, r, logic.FormatError(err, "badrequest"))
+		slog.Error("peer not found: ", "publickey", failOverReq.PeerPubKey, "error", err)
 		return
+	}
+	peerNodeMap := make(map[string]models.Node)
+	for _, nodeID := range peerHost.Nodes {
+		node, err := logic.GetNodeByID(nodeID)
+		if err != nil {
+			continue
+		}
+		peerNodeMap[node.Network] = node
+	}
+	for _, nodeID := range host.Nodes {
+		node, err := logic.GetNodeByID(nodeID)
+		if err != nil {
+			slog.Error("couldn't find node", "id", nodeID, "error", err)
+			continue
+		}
+		if node.IsRelayed {
+			continue
+		}
+		peerNode, ok := peerNodeMap[node.Network]
+		if !ok {
+			continue
+		}
+		// get auto relay Host in this network
+		failOverNode, err := proLogic.GetFailOverNode(node.Network, allNodes)
+		if err != nil {
+			slog.Error("auto relay not found", "network", node.Network)
+			continue
+		}
+		err = proLogic.SetFailOverCtx(failOverNode, node, peerNode)
+		if err != nil {
+			slog.Error("failed to create relay:", "id", node.ID.String(),
+				"network", node.Network, "error", err)
+			continue
+		}
+		slog.Info("[auto-relay] created relay on node", "node", node.ID.String(), "network", node.Network)
+		sendPeerUpdate = true
 	}
 
-	if peerNode.Network != node.Network {
-		slog.Error("node and peer aren't of same network", "error", err.Error())
-		logic.ReturnErrorResponse(w, r, logic.FormatError(err, "badrequest"))
-		return
+	if sendPeerUpdate {
+		go mq.PublishPeerUpdate()
 	}
-	if node.IsRelayed {
-		logic.ReturnErrorResponse(w, r, logic.FormatError(err, "badrequest"))
-		return
-	}
-	failOverNode, err := proLogic.GetFailOverNode(node.Network, allNodes)
-	if err != nil {
-		slog.Error("auto relay not found", "network", node.Network)
-		logic.ReturnErrorResponse(w, r, logic.FormatError(err, "internal"))
-		return
-	}
-	err = proLogic.SetFailOverCtx(failOverNode, node, peerNode)
-	if err != nil {
-		slog.Error("failed to create relay:", "id", node.ID.String(),
-			"network", node.Network, "error", err)
-		logic.ReturnErrorResponse(w, r, logic.FormatError(err, "internal"))
-		return
-	}
-	slog.Info("[auto-relay] created relay on node", "node", node.ID.String(), "network", node.Network)
-	go mq.PublishPeerUpdate()
+
 	w.Header().Set("Content-Type", "application/json")
 	logic.ReturnSuccessResponse(w, r, "relayed successfully")
 }
