0fc756c77497377eef26a9603c67fe4e1efadba3
abhishek9686 <abhi281342@gmail.com>
2024-03-18 17:43:49 +0700

    add emqx hosts del api to limited middleware controller

--- pro/controllers/middleware.go
+++ pro/controllers/middleware.go
@@ -1,16 +1,24 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gravitl/netmaker/logic"
 	"github.com/gravitl/netmaker/servercfg"
-	"net/http"
 )
+
+var limitedApis = map[string]struct{}{
+	"/api/server/status": {},
+	"/api/emqx/hosts":    {},
+}
 
 func OnlyServerAPIWhenUnlicensedMiddleware(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		if servercfg.ErrLicenseValidation != nil && request.URL.Path != "/api/server/status" {
-			logic.ReturnErrorResponse(writer, request, logic.FormatError(servercfg.ErrLicenseValidation, "forbidden"))
-			return
+		if servercfg.ErrLicenseValidation != nil {
+			if _, ok := limitedApis[request.URL.Path]; !ok {
+				logic.ReturnErrorResponse(writer, request, logic.FormatError(servercfg.ErrLicenseValidation, "forbidden"))
+				return
+			}
 		}
 		handler.ServeHTTP(writer, request)
 	})
