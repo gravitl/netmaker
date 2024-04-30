425359cc6658f6b53bcbfcbf650f92dd9db34804
momantech <cuimoman@qq.com>
2024-05-01 05:40:41 +0800

    chore: fix some comments

--- logic/version_test.go
+++ logic/version_test.go
@@ -9,27 +9,27 @@ import (
 func TestVersion(t *testing.T) {
 	t.Run("valid version", func(t *testing.T) {
 		is := is.New(t)
-		valid := IsVersionComptatible("v0.17.1-testing")
+		valid := IsVersionCompatible("v0.17.1-testing")
 		is.Equal(valid, true)
 	})
 	t.Run("dev version", func(t *testing.T) {
 		is := is.New(t)
-		valid := IsVersionComptatible("dev")
+		valid := IsVersionCompatible("dev")
 		is.Equal(valid, true)
 	})
 	t.Run("invalid version", func(t *testing.T) {
 		is := is.New(t)
-		valid := IsVersionComptatible("v0.14.2-refactor")
+		valid := IsVersionCompatible("v0.14.2-refactor")
 		is.Equal(valid, false)
 	})
 	t.Run("no version", func(t *testing.T) {
 		is := is.New(t)
-		valid := IsVersionComptatible("testing")
+		valid := IsVersionCompatible("testing")
 		is.Equal(valid, false)
 	})
 	t.Run("incomplete version", func(t *testing.T) {
 		is := is.New(t)
-		valid := IsVersionComptatible("0.18")
+		valid := IsVersionCompatible("0.18")
 		is.Equal(valid, true)
 	})
 }
