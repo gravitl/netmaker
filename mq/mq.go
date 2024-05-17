3ed183344b8f3d313bb8f1ef71cecd13cb0b7077
abhishek9686 <abhi281342@gmail.com>
2024-05-17 11:28:53 +0530

    send peer update on startup

--- mq/mq.go
+++ mq/mq.go
@@ -129,6 +129,7 @@ func SetupMQTT(fatal bool) {
 
 // Keepalive -- periodically pings all nodes to let them know server is still alive and doing well
 func Keepalive(ctx context.Context) {
+	go PublishPeerUpdate(false)
 	for {
 		select {
 		case <-ctx.Done():
