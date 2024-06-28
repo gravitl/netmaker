3ca8df0cf5cfc7326a42edb02e12ffc1d3dbf656
Max Ma <mayabin@gmail.com>
2024-06-28 09:38:49 +0200

    revert the peerUpdate from SignalPull

--- mq/publishers.go
+++ mq/publishers.go
@@ -24,19 +24,17 @@ func PublishPeerUpdate(replacePeers bool) error {
 		logger.Log(1, "err getting all hosts", err.Error())
 		return err
 	}
-
+	allNodes, err := logic.GetAllNodes()
+	if err != nil {
+		return err
+	}
 	for _, host := range hosts {
 		host := host
 		go func(host models.Host) {
-			hostUpdate := models.HostUpdate{
-				Action: models.SignalPull,
-				Host:   host,
-			}
-			if err = HostUpdate(&hostUpdate); err != nil {
-				slog.Warn("failed to send host pull request", "host", host.ID.String(), "error", err)
+			if err = PublishSingleHostPeerUpdate(&host, allNodes, nil, nil, replacePeers); err != nil {
+				logger.Log(1, "failed to publish peer update to host", host.ID.String(), ": ", err.Error())
 			}
 		}(host)
-
 	}
 	return err
 }
