10de931a866f744bc83ffdaf822694991e827e33
abhishek9686 <abhi281342@gmail.com>
2024-10-24 15:16:59 +0400

    fix extclient validation err messages

--- controllers/regex.go
+++ controllers/regex.go
@@ -7,9 +7,9 @@ import (
 
 var (
 	errInvalidExtClientPubKey  = errors.New("incorrect ext client public key")
-	errInvalidExtClientID      = errors.New("ext client ID must be alphanumderic and/or dashes and less that 15 chars")
-	errInvalidExtClientExtraIP = errors.New("ext client extra ip must be a valid cidr")
-	errInvalidExtClientDNS     = errors.New("ext client dns must be a valid ip address")
+	errInvalidExtClientID      = errors.New("client ID must be alphanumderic and/or dashes and less that 15 chars")
+	errInvalidExtClientExtraIP = errors.New("client extra ip must be a valid cidr")
+	errInvalidExtClientDNS     = errors.New("client dns must be a valid ip address")
 	errDuplicateExtClientName  = errors.New("duplicate client name")
 )
 
