79048868d9988fc7bd2aa086f035936aae4ce6c0
abhishek9686 <abhi281342@gmail.com>
2024-10-25 16:17:55 +0400

    fix client validation messages

--- controllers/regex.go
+++ controllers/regex.go
@@ -6,8 +6,8 @@ import (
 )
 
 var (
-	errInvalidExtClientPubKey  = errors.New("incorrect ext client public key")
-	errInvalidExtClientID      = errors.New("client ID must be alphanumderic and/or dashes and less that 15 chars")
+	errInvalidExtClientPubKey  = errors.New("incorrect client public key")
+	errInvalidExtClientID      = errors.New("node name must be alphanumderic and/or dashes and less that 15 chars")
 	errInvalidExtClientExtraIP = errors.New("client extra ip must be a valid cidr")
 	errInvalidExtClientDNS     = errors.New("client dns must be a valid ip address")
 	errDuplicateExtClientName  = errors.New("duplicate client name")
