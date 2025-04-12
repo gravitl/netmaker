9cbb7f35434b26f25cdce81c40a32fbdad45fd4e
abhishek9686 <abhi281342@gmail.com>
2025-04-12 14:06:10 +0400

    update egress model

--- models/egress.go
+++ models/egress.go
@@ -3,10 +3,10 @@ package models
 import "net"
 
 type Egress struct {
-	ID           string    `json:"id"`
-	Name         string    `json:"name"`
-	Description  string    `json:"description"`
-	RoutingNode  string    `json:"routing_node"`
-	RoutingGroup []TagID   `json:"routing_tags"`
-	Range        net.IPNet `json:"range"`
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	EgressNode  string    `json:"egress_node"`
+	EgressGroup []TagID   `json:"egress_group"`
+	Range       net.IPNet `json:"range"`
 }
