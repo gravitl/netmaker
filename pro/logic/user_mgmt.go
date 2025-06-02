f88b37313ed572f49e79774a1df5494e5f63c88d
abhishek9686 <abhi281342@gmail.com>
2025-06-02 14:44:18 +0530

    fix default network user policy

--- pro/logic/user_mgmt.go
+++ pro/logic/user_mgmt.go
@@ -1245,7 +1245,7 @@ func CreateDefaultUserPolicies(netID models.NetworkID) {
 			Src: []models.AclPolicyTag{
 				{
 					ID:    models.UserGroupAclID,
-					Value: globalNetworksAdminGroupID.String(),
+					Value: globalNetworksUserGroupID.String(),
 				},
 				{
 					ID:    models.UserGroupAclID,
