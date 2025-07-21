1443961064cac600fa542ba833e098b5443fba4d
Vishal Dalwadi <dalwadivishal26@gmail.com>
2025-07-21 14:20:18 +0530

    fix(go): use correct method for pro;
    fixes: User Config nodes are always reported online.

--- logic/nodes.go
+++ logic/nodes.go
@@ -471,7 +471,7 @@ func AddStatusToNodes(nodes []models.Node, statusCall bool) (nodesWithStatus []m
 		if statusCall {
 			GetNodeStatus(&node, aclDefaultPolicyStatusMap[node.Network])
 		} else {
-			GetNodeCheckInStatus(&node, true)
+			getNodeCheckInStatus(&node, true)
 		}
 
 		nodesWithStatus = append(nodesWithStatus, node)
