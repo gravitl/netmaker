d4c778999c9e4a1ad2e8c6155ab44c925a481747
abhishek9686 <abhi281342@gmail.com>
2025-08-15 00:23:40 +0530

    send peer update domain resolution update

--- controllers/hosts.go
+++ controllers/hosts.go
@@ -393,6 +393,7 @@ func hostUpdateFallback(w http.ResponseWriter, r *http.Request) {
 			e.Range = strings.Join(hostUpdate.Node.EgressGatewayRanges, ",")
 			e.Update(db.WithContext(r.Context()))
 		}
+		sendPeerUpdate = true
 	}
 
 	if sendPeerUpdate {
