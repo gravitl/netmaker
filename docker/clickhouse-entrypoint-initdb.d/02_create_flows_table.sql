2a9f0d65112036efbe15d9539417356ed53da218
VishalDalwadi <dalwadivishal26@gmail.com>
2025-12-01 23:23:17 +0530

    feat(go): use node instead of host as type; optimize protobuf defs;

--- docker/clickhouse-entrypoint-initdb.d/02_create_flows_table.sql
+++ docker/clickhouse-entrypoint-initdb.d/02_create_flows_table.sql
@@ -16,11 +16,11 @@ CREATE TABLE IF NOT EXISTS flows (
 
     -- Participants
     src_ip             String,
-    src_type           Enum8('host'=1,'user'=2,'extclient'=3,'external'=4),
+    src_type           Enum8('node'=1,'user'=2,'extclient'=3,'external'=4),
     src_entity_id      String,
 
     dst_ip             String,
-    dst_type           Enum8('host'=1,'user'=2,'extclient'=3,'external'=4),
+    dst_type           Enum8('node'=1,'user'=2,'extclient'=3,'external'=4),
     dst_entity_id      String,
 
     -- Timestamps
