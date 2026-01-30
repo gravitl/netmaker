8521ba681db20f14384b5d0d7b961806fc9118f7
abhishek9686 <abhi281342@gmail.com>
2026-01-30 18:47:10 +0400

    NM-213: update jit expiry on existing clients

--- controllers/ext_client.go
+++ controllers/ext_client.go
@@ -704,7 +704,10 @@ func createExtClient(w http.ResponseWriter, r *http.Request) {
 			for _, extclient := range extclients {
 				if extclient.DeviceID == customExtClient.DeviceID &&
 					extclient.OwnerID == caller.UserName && nodeid == extclient.IngressGatewayID {
-
+					if jitGrant != nil {
+						extclient.JITExpiresAt = &jitGrant.ExpiresAt
+						_ = logic.SaveExtClient(&extclient)
+					}
 					err = errors.New("remote client config already exists on the gateway")
 					slog.Error("failed to create extclient", "user", userName, "error", err)
 					logic.ReturnErrorResponse(w, r, logic.FormatError(err, "badrequest"))
