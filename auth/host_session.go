30bf191dd65550c6da1b0274241c86fac4b883f0
VishalDalwadi <dalwadivishal26@gmail.com>
2026-03-19 14:02:44 +0530

    fix(go): set persistent keep alive when registering host using sso;

--- auth/host_session.go
+++ auth/host_session.go
@@ -163,6 +163,7 @@ func SessionHandler(conn *websocket.Conn) {
 	case result := <-answer: // a read from req.answerCh has occurred
 		// add the host, if not exists, handle like enrollment registration
 		if !logic.HostExists(&result.Host) { // check if host already exists, add if not
+			result.Host.PersistentKeepalive = models.DefaultPersistentKeepAlive
 			if servercfg.GetBrokerType() == servercfg.EmqxBrokerType {
 				if err := mq.GetEmqxHandler().CreateEmqxUser(result.Host.ID.String(), result.Host.HostPass); err != nil {
 					logger.Log(0, "failed to create host credentials for EMQX: ", err.Error())
