3f91bf4b9748b6edea96f7101669dec36754803f
abhishek9686 <abhi281342@gmail.com>
2026-05-07 15:54:30 +0530

    NM-272: run each migration step in its own independent transaction, committing before moving to the next

--- migrate/migrate_schema.go
+++ migrate/migrate_schema.go
@@ -16,8 +16,26 @@ type migrationFunc func(ctx context.Context) error
 // ToSQLSchema migrates the data from key-value
 // db to sql db.
 func ToSQLSchema() error {
-	// begin a new transaction.
-	dbctx := db.BeginTx(context.TODO())
+	// v1.5.1 migration includes migrating the users, groups, roles, networks and hosts tables.
+	// future table migrations should be made below this block,
+	// with a different version number and a similar check for whether the
+	// migration was already done.
+	err := ensureMigrationCompleted(context.TODO(), "migration-v1.5.1", migrateV1_5_1)
+	if err != nil {
+		return err
+	}
+
+	// v1.5.2 migration includes migrating the pending users and user invites tables.
+	err = ensureMigrationCompleted(context.TODO(), "migration-v1.5.2", migrateV1_5_2)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ensureMigrationCompleted(ctx context.Context, version string, migrate migrationFunc) error {
+	dbctx := db.BeginTx(ctx)
 	commit := false
 	defer func() {
 		if commit {
@@ -27,30 +45,10 @@ func ToSQLSchema() error {
 		}
 	}()
 
-	// v1.5.1 migration includes migrating the users, groups, roles, networks and hosts tables.
-	// future table migrations should be made below this block,
-	// with a different version number and a similar check for whether the
-	// migration was already done.
-	err := ensureMigrationCompleted(dbctx, "migration-v1.5.1", migrateV1_5_1)
-	if err != nil {
-		return err
-	}
-
-	// v1.5.2 migration includes migrating the pending users and user invites tables.
-	err = ensureMigrationCompleted(dbctx, "migration-v1.5.2", migrateV1_5_2)
-	if err != nil {
-		return err
-	}
-
-	commit = true
-	return nil
-}
-
-func ensureMigrationCompleted(ctx context.Context, version string, migrate migrationFunc) error {
 	migrationJob := &schema.Job{
 		ID: version,
 	}
-	err := migrationJob.Get(ctx)
+	err := migrationJob.Get(dbctx)
 	if err != nil {
 		if !errors.Is(err, gorm.ErrRecordNotFound) {
 			return err
@@ -58,13 +56,13 @@ func ensureMigrationCompleted(ctx context.Context, version string, migrate migra
 
 		logger.Log(1, fmt.Sprintf("running migration job %s", migrationJob.ID))
 		// migrate.
-		err = migrate(ctx)
+		err = migrate(dbctx)
 		if err != nil {
 			return err
 		}
 
 		// mark migration job completed.
-		err = migrationJob.Create(ctx)
+		err = migrationJob.Create(dbctx)
 		if err != nil {
 			return err
 		}
@@ -74,6 +72,7 @@ func ensureMigrationCompleted(ctx context.Context, version string, migrate migra
 		logger.Log(1, fmt.Sprintf("migration job %s already completed, skipping", migrationJob.ID))
 	}
 
+	commit = true
 	return nil
 }
 
