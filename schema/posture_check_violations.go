f6fb80a79975679b4bce2c7828a637c4e100cb9b
abhishek9686 <abhi281342@gmail.com>
2026-05-07 16:41:12 +0530

    NM-293: fix invalid gorm struct tags

--- schema/posture_check_violations.go
+++ schema/posture_check_violations.go
@@ -7,9 +7,9 @@ import (
 const postureCheckViolationsTable = "posture_check_violations_v1"
 
 type PostureCheckViolation struct {
-	EvaluationCycleID string    `gorm:"primaryKey" json:"evaluation_cycle_id"`
-	CheckID           string    `gorm:"primaryKey" json:"check_id"`
-	NodeID            string    `gorm:"primaryKey" json:"node_id"`
+	EvaluationCycleID string    `gorm:"primaryKey;column:evaluation_cycle_id" json:"evaluation_cycle_id"`
+	CheckID           string    `gorm:"primaryKey;column:check_id" json:"check_id"`
+	NodeID            string    `gorm:"primaryKey;column:node_id" json:"node_id"`
 	Name              string    `json:"name"`
 	Attribute         string    `json:"attribute"`
 	Message           string    `json:"message"`
