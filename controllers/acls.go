b7c2666c6eb95e1fa18befa05bd7a107b7885b28
Abhishek Kondur <abhi281342@gmail.com>
2026-05-15 21:49:59 +0530

    NM-298: JIT group scoped (#4005)
    * NM-298: JIT group scoped
    
    * NM-298: add JIT activity logs
    
    * feat(go): log enable/disable acl policy;
    
    * feat(go): log enable/disable user;
    
    * NM-298: reset jit expiry field on user config if disabled
    
    * NM-298: add target user for jit actionm events
    
    * NM-298: fix jit grps cleanup
    
    * NM-298: fix user to admin role jit grp cleanup, add grp name jit event grp update
    
    ---------
    
    Co-authored-by: VishalDalwadi <dalwadivishal26@gmail.com>

--- controllers/acls.go
+++ controllers/acls.go
@@ -365,6 +365,14 @@ func updateAcl(w http.ResponseWriter, r *http.Request) {
 		logic.ReturnErrorResponse(w, r, logic.FormatError(err, "badrequest"))
 		return
 	}
+
+	action := schema.Update
+	if updateAcl.Enabled && !acl.Enabled {
+		action = schema.EnableAclPolicy
+	} else if !updateAcl.Enabled && acl.Enabled {
+		action = schema.DisableAclPolicy
+	}
+
 	if err := logic.NormalizeAndValidateAclEgressIPs(&updateAcl.Acl); err != nil {
 		logic.ReturnErrorResponse(w, r, logic.FormatError(err, "badrequest"))
 		return
@@ -387,7 +395,7 @@ func updateAcl(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	logic.LogEvent(&models.Event{
-		Action: schema.Update,
+		Action: action,
 		Source: models.Subject{
 			ID:   r.Header.Get("user"),
 			Name: r.Header.Get("user"),
