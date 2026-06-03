641aa7bec3e1c5ab6713cfbcfb0500f389f11c1b
VishalDalwadi <dalwadivishal26@gmail.com>
2026-06-03 11:24:12 +0530

    fix(go): set expiration datetime only if non-zero;

--- models/api_node.go
+++ models/api_node.go
@@ -142,7 +142,11 @@ func (a *ApiNode) ConvertToServerNode(currentNode *Node) *Node {
 	convertedNode.LastModified = time.Unix(a.LastModified, 0)
 	convertedNode.LastCheckIn = time.Unix(a.LastCheckIn, 0)
 	convertedNode.LastPeerUpdate = time.Unix(a.LastPeerUpdate, 0)
-	convertedNode.ExpirationDateTime = currentNode.ExpirationDateTime
+	if a.ExpirationDateTime == 0 {
+		convertedNode.ExpirationDateTime = currentNode.ExpirationDateTime
+	} else {
+		convertedNode.ExpirationDateTime = time.Unix(a.ExpirationDateTime, 0)
+	}
 	convertedNode.Metadata = a.Metadata
 	for _, ip := range a.AdditionalRagIps {
 		ragIp := net.ParseIP(ip)
