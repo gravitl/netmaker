fe57ac175d85979ae869e77a80a5f3409d6a64b7
VishalDalwadi <dalwadivishal26@gmail.com>
2026-06-04 17:01:17 +0530

    fix(go): add type to dns entry model;

--- models/dnsEntry.go
+++ models/dnsEntry.go
@@ -40,12 +40,20 @@ type DNSUpdate struct {
 	NewAddress string
 }
 
+type DNSEntryType string
+
+const (
+	DNSEntryType_Node   = "node"
+	DNSEntryType_Custom = "custom"
+)
+
 // DNSEntry - a DNS entry represented as struct
 type DNSEntry struct {
-	Address  string `json:"address" validate:"omitempty,ip"`
-	Address6 string `json:"address6" validate:"omitempty,ip"`
-	Name     string `json:"name" validate:"required,name_unique,min=1,max=192,whitespace"`
-	Network  string `json:"network" validate:"network_exists"`
+	Type     DNSEntryType `json:"type"`
+	Address  string       `json:"address" validate:"omitempty,ip"`
+	Address6 string       `json:"address6" validate:"omitempty,ip"`
+	Name     string       `json:"name" validate:"required,name_unique,min=1,max=192,whitespace"`
+	Network  string       `json:"network" validate:"network_exists"`
 }
 
 type NameserverReq struct {
