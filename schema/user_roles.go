297b6567bbca41e9fdca151643c4680e59dc4c97
VishalDalwadi <dalwadivishal26@gmail.com>
2026-06-22 23:01:53 +0530

    wip(go): remove tenant id column from user role table;

--- schema/user_roles.go
+++ schema/user_roles.go
@@ -113,7 +113,6 @@ type ResourceAccess map[RsrcType]map[RsrcID]RsrcPermissionScope
 
 type UserRole struct {
 	ID                  UserRoleID                         `gorm:"primaryKey" json:"id"`
-	TenantID            string                             `gorm:"default:''" json:"tenant_id"`
 	Name                string                             `json:"name"`
 	Default             bool                               `json:"default"`
 	MetaData            string                             `json:"meta_data"`
