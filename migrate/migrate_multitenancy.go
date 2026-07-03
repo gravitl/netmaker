3808a44404d1af51a6646753e6b70dd548f7cb02
VishalDalwadi <dalwadivishal26@gmail.com>
2026-07-03 23:03:22 +0530

    wip(go): always create default org and tenant;

--- migrate/migrate_multitenancy.go
+++ migrate/migrate_multitenancy.go
@@ -3,7 +3,6 @@ package migrate
 import (
 	"context"
 
-	"github.com/gravitl/netmaker/db"
 	"github.com/gravitl/netmaker/schema"
 )
 
@@ -35,32 +34,33 @@ func createDefaults(ctx context.Context) error {
 }
 
 func isNewDeployment(ctx context.Context) (bool, error) {
-	if db.FromContext(ctx).Migrator().HasTable(TableName_Users) {
-		numUsers, err := kvCount(ctx, TableName_Users)
-		if err != nil {
-			return false, err
-		}
-
-		if numUsers == 0 {
-			numUsers, err = (&schema.User{}).Count(ctx)
-			if err != nil {
-				return false, err
-			}
-
-			if numUsers == 0 {
-				return true, nil
-			}
-		}
-	} else {
-		numUsers, err := (&schema.User{}).Count(ctx)
-		if err != nil {
-			return false, err
-		}
-
-		if numUsers == 0 {
-			return true, nil
-		}
-	}
+	// todo: uncomment when context is wired everywhere
+	//if db.FromContext(ctx).Migrator().HasTable(TableName_Users) {
+	//	numUsers, err := kvCount(ctx, TableName_Users)
+	//	if err != nil {
+	//		return false, err
+	//	}
+	//
+	//	if numUsers == 0 {
+	//		numUsers, err = (&schema.User{}).Count(ctx)
+	//		if err != nil {
+	//			return false, err
+	//		}
+	//
+	//		if numUsers == 0 {
+	//			return true, nil
+	//		}
+	//	}
+	//} else {
+	//	numUsers, err := (&schema.User{}).Count(ctx)
+	//	if err != nil {
+	//		return false, err
+	//	}
+	//
+	//	if numUsers == 0 {
+	//		return true, nil
+	//	}
+	//}
 
 	return false, nil
 }
