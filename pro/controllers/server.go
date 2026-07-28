25883e0044aa605a675e7d3d0de2f3281c248bac
VishalDalwadi <dalwadivishal26@gmail.com>
2026-07-28 17:13:16 +0530

    wip(go): org scope license validation;

--- pro/controllers/server.go
+++ pro/controllers/server.go
@@ -6,11 +6,13 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/gravitl/netmaker/logic"
+	"github.com/gravitl/netmaker/middleware"
 	"github.com/gravitl/netmaker/pro/license"
+	"github.com/gravitl/netmaker/scope"
 )
 
 func ServerHandlers(r *mux.Router) {
-	r.HandleFunc("/api/v1/server/license/validation", logic.SecurityCheck(true, http.HandlerFunc(triggerLicenseValidation))).Methods(http.MethodPost)
+	r.HandleFunc("/api/v1/server/license/validation", middleware.Scope(scope.OrgScope, logic.SecurityCheck(true, http.HandlerFunc(triggerLicenseValidation)))).Methods(http.MethodPost)
 }
 
 func triggerLicenseValidation(w http.ResponseWriter, r *http.Request) {
