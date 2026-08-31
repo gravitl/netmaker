368f4ec606ca87f40a0c079f6393cc50f617e7db
Abhishek Kondur <abhi98.kondur@gmail.com>
2026-08-31 18:57:23 +0530

    v1.7.0: update release notes

--- main.go
+++ main.go
@@ -38,10 +38,10 @@ import (
 	"golang.org/x/exp/slog"
 )
 
-var version = "v1.6.0"
+var version = "v1.7.0"
 
 //	@title			NetMaker
-//	@version		1.6.0
+//	@version		1.7.0
 //	@description	NetMaker API Docs
 //	@tag.name	    APIUsage
 //	@tag.description.markdown
