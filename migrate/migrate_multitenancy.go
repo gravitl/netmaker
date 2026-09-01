bf5489105ffd702cfe9a51329ffdaf0383fff5f0
Vishal Dalwadi <dalwadivishal26@gmail.com>
2026-09-01 23:27:36 +0530

    feat(sh): rekey tenant scoped keys as well;

--- migrate/migrate_multitenancy.go
+++ migrate/migrate_multitenancy.go
@@ -64,9 +64,131 @@ func scopedModels() []any {
 	return []any{&schema.PendingUser{}, &schema.UserInvite{}}
 }
 
+func rekeyTenantScopedKeys(ctx context.Context, oldID, newID string) error {
+	var userGroupIDs []string
+	if err := db.FromContext(ctx).Model(&schema.UserGroup{}).
+		Where("tenant_id = ?", oldID).
+		Pluck("id", &userGroupIDs).Error; err != nil {
+		return err
+	}
+	for _, id := range userGroupIDs {
+		logicalID := schema.UnscopeUserGroupID(oldID, schema.UserGroupID(id))
+		newKey := schema.ScopeUserGroupID(newID, logicalID).String()
+		if newKey == id {
+			continue
+		}
+		if err := db.FromContext(ctx).Model(&schema.UserGroup{}).
+			Where("id = ?", id).
+			Updates(map[string]any{"id": newKey, "tenant_id": newID}).Error; err != nil {
+			return err
+		}
+	}
+
+	var tagKeys []string
+	if err := db.FromContext(ctx).Model(&schema.TagRecord{}).
+		Where("tenant_id = ?", oldID).
+		Pluck("key", &tagKeys).Error; err != nil {
+		return err
+	}
+	for _, key := range tagKeys {
+		newKey := schema.TenantScopedKey(newID, schema.StripTenantKey(oldID, key))
+		if newKey == key {
+			continue
+		}
+		if err := db.FromContext(ctx).Model(&schema.TagRecord{}).
+			Where("key = ?", key).
+			Updates(map[string]any{"key": newKey, "tenant_id": newID}).Error; err != nil {
+			return err
+		}
+	}
+
+	var extClientKeys []string
+	if err := db.FromContext(ctx).Model(&schema.ExtClientRecord{}).
+		Where("tenant_id = ?", oldID).
+		Pluck("key", &extClientKeys).Error; err != nil {
+		return err
+	}
+	for _, key := range extClientKeys {
+		newKey := schema.TenantScopedKey(newID, schema.StripTenantKey(oldID, key))
+		if newKey == key {
+			continue
+		}
+		if err := db.FromContext(ctx).Model(&schema.ExtClientRecord{}).
+			Where("key = ?", key).
+			Updates(map[string]any{"key": newKey, "tenant_id": newID}).Error; err != nil {
+			return err
+		}
+	}
+
+	var dnsKeys []string
+	if err := db.FromContext(ctx).Model(&schema.DNSRecord{}).
+		Where("tenant_id = ?", oldID).
+		Pluck("key", &dnsKeys).Error; err != nil {
+		return err
+	}
+	for _, key := range dnsKeys {
+		newKey := schema.TenantScopedKey(newID, schema.StripTenantKey(oldID, key))
+		if newKey == key {
+			continue
+		}
+		if err := db.FromContext(ctx).Model(&schema.DNSRecord{}).
+			Where("key = ?", key).
+			Updates(map[string]any{"key": newKey, "tenant_id": newID}).Error; err != nil {
+			return err
+		}
+	}
+
+	var aclKeys []string
+	if err := db.FromContext(ctx).Model(&schema.AclRecord{}).
+		Where("tenant_id = ?", oldID).
+		Pluck("key", &aclKeys).Error; err != nil {
+		return err
+	}
+	for _, key := range aclKeys {
+		newKey := schema.TenantScopedKey(newID, schema.StripTenantKey(oldID, key))
+		if newKey == key {
+			continue
+		}
+		if err := db.FromContext(ctx).Model(&schema.AclRecord{}).
+			Where("key = ?", key).
+			Updates(map[string]any{"key": newKey, "tenant_id": newID}).Error; err != nil {
+			return err
+		}
+	}
+
+	roleQuery := db.FromContext(ctx).Model(&schema.UserRole{}).Where("network_id <> ''")
+	if oldID == "" {
+		roleQuery = roleQuery.Where("id NOT LIKE '%::%'")
+	} else {
+		roleQuery = roleQuery.Where("id LIKE ?", oldID+"::%")
+	}
+	var roleIDs []string
+	if err := roleQuery.Pluck("id", &roleIDs).Error; err != nil {
+		return err
+	}
+	for _, id := range roleIDs {
+		logicalID := schema.UnscopeUserRoleID(oldID, schema.UserRoleID(id))
+		newKey := schema.ScopeUserRoleID(newID, logicalID).String()
+		if newKey == id {
+			continue
+		}
+		if err := db.FromContext(ctx).Model(&schema.UserRole{}).
+			Where("id = ?", id).
+			Update("id", newKey).Error; err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func RekeyTenant(ctx context.Context, oldID, newID string) error {
 	if oldID == newID {
 		return nil
+	}
+
+	if err := rekeyTenantScopedKeys(ctx, oldID, newID); err != nil {
+		return err
 	}
 
 	models := append(tenantScopedModels(), &schema.TenantMembership{})
