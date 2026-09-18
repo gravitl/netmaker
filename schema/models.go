08f85b882eff78040975c27cdc45da0e841a3920
Vishal Dalwadi <dalwadivishal26@gmail.com>
2026-09-18 11:15:58 +0530

    wip(go): auto migrate only new extclient table;

--- schema/models.go
+++ schema/models.go
@@ -35,7 +35,7 @@ func ListModels() []interface{} {
 		&AclRecord{},
 		&CacheRecord{},
 		&DNSRecord{},
-		&ExtClientRecord{},
+		&ExtClientV1{},
 		&MetricsRecord{},
 		&SsoStateRecord{},
 		&TagRecord{},
