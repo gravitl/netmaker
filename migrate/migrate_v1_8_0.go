cf0620cbf8bf4160568e90e8b8f139f77b7797d3
Vishal Dalwadi <dalwadivishal26@gmail.com>
2026-09-24 00:19:25 +0530

    feat(go): add user roles migration and test;

--- migrate/migrate_v1_8_0.go
+++ migrate/migrate_v1_8_0.go
@@ -2,17 +2,28 @@ package migrate
 
 import (
 	"context"
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/gravitl/netmaker/db"
 	"github.com/gravitl/netmaker/migrate/types"
+	"github.com/gravitl/netmaker/orchestrator"
 	"github.com/gravitl/netmaker/schema"
+	"github.com/gravitl/netmaker/scope"
 	"gorm.io/datatypes"
 )
 
+const TableName_ExtClients = "extclients"
+
 func migrateV1_8_0(ctx context.Context) error {
-	return migrateExtClientsToSQLTable(ctx)
+	err := migrateExtClientsToSQLTable(ctx)
+	if err != nil {
+		return err
+	}
+
+	return migrateUserRolesToScoped(ctx)
 }
 
 func migrateExtClientsToSQLTable(ctx context.Context) error {
@@ -105,6 +116,52 @@ func migrateExtClientsToSQLTable(ctx context.Context) error {
 		}
 
 		if err := db.FromContext(ctx).Model(&schema.Extclient{}).Create(v1).Error; err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func migrateUserRolesToScoped(ctx context.Context) error {
+	tx := db.FromContext(ctx)
+	if !tx.Migrator().HasTable("user_roles_v1") {
+		return nil
+	}
+
+	var rows []schema.UserRole
+	if err := tx.Find(&rows).Error; err != nil {
+		return err
+	}
+
+	for _, r := range rows {
+		oldID := r.ID.String()
+		update := map[string]interface{}{}
+
+		if idx := strings.Index(oldID, "::"); idx != -1 {
+			update["scope"] = scope.TenantScope
+			update["scope_id"] = oldID[:idx]
+			if r.Name == "" {
+				logicalID := oldID[idx+2:]
+				admin, err := schema.NetworkRoleTypeFromLegacyID(logicalID)
+				if err != nil {
+					return fmt.Errorf("migrate user role %q: %w", oldID, err)
+				}
+				update["name"] = schema.NetworkRoleDisplayName(r.NetworkID, admin)
+			}
+		} else if r.Default && r.Name == "" {
+			update["name"] = oldID
+			if orchestrator.ValidOrgRoles[r.ID] {
+				update["scope"] = scope.OrgScope
+			} else {
+				update["scope"] = scope.TenantScope
+			}
+		} else {
+			continue
+		}
+
+		update["id"] = uuid.NewString()
+		if err := tx.Model(&schema.UserRole{}).Where("id = ?", oldID).Updates(update).Error; err != nil {
 			return err
 		}
 	}
